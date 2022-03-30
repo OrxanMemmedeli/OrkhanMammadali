d1666715e5d838c67428ead653cec2077b5858d4
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-03-30 13:23:52 +0400

    Add layers and injectionService for about entity

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void Delete(About t)
+        {
+            _aboutDal.Delete(t);
+        }
+
+        public About Get(Expression<Func<About, bool>> filter)
+        {
+            return _aboutDal.Get(filter);
+        }
+
+        public List<About> GetAll()
+        {
+            return _aboutDal.GetAll();
+        }
+
+        public List<About> GetAll(Expression<Func<About, bool>> filter)
+        {
+            return _aboutDal.GetAll(filter);
+        }
+
+        public About GetById(int id)
+        {
+            return _aboutDal.GetById(id);
+        }
+
+        public void Insert(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void Update(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
--- OrkhanMammadali/Startup.cs
+++ OrkhanMammadali/Startup.cs
@@ -1,3 +1,4 @@
+using DataAccessLayer.Contrete.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace OrkhanMammadali
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.Register();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

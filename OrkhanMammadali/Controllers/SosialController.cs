a4ebc4c85b1184d25e49c5e9606cbe11eddcaa5c
Orxan Məmmədəli <memmedeli.orxan.om@gmail.com>
2022-05-06 12:40:39 +0400

    Add controller and View for UI

--- OrkhanMammadali/Controllers/SosialController.cs
+++ OrkhanMammadali/Controllers/SosialController.cs
@@ -11,11 +11,11 @@ using System.Threading.Tasks;
 
 namespace OrkhanMammadali.Controllers
 {
-    public class AboutController : Controller
+    public class SosialController : Controller
     {
         private readonly IConfiguration _configuration;
         private readonly string url;
-        public AboutController(IConfiguration configuration)
+        public SosialController(IConfiguration configuration)
         {
             _configuration = configuration;
             url = _configuration["MyAPI"];
@@ -25,9 +25,9 @@ namespace OrkhanMammadali.Controllers
         public async Task<IActionResult> Index()
         {
             var httpclient = new HttpClient();
-            var responseMessage = await httpclient.GetAsync(url + "api/About");
+            var responseMessage = await httpclient.GetAsync(url + "api/Sosial");
             var jsonString = await responseMessage.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<List<AboutViewModel>>(jsonString);
+            var values = JsonConvert.DeserializeObject<List<SosialViewModel>>(jsonString);
             return View(values);
         }
 
@@ -36,12 +36,12 @@ namespace OrkhanMammadali.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Create(AboutViewModel model)
+        public async Task<IActionResult> Create(SosialViewModel model)
         {
             var httpclient = new HttpClient();
             var jsonModel = JsonConvert.SerializeObject(model);
             StringContent content = new StringContent(jsonModel, Encoding.UTF8, "application/json");
-            var responseMessage = await httpclient.PostAsync(url + "api/About", content);
+            var responseMessage = await httpclient.PostAsync(url + "api/Sosial", content);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
@@ -58,11 +58,11 @@ namespace OrkhanMammadali.Controllers
             }
 
             var httpclient = new HttpClient();
-            var responseMessage = await httpclient.GetAsync(url + "api/About/" + id);
+            var responseMessage = await httpclient.GetAsync(url + "api/Sosial/" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonModel = await responseMessage.Content.ReadAsStringAsync();
-                var value = JsonConvert.DeserializeObject<AboutViewModel>(jsonModel);
+                var value = JsonConvert.DeserializeObject<SosialViewModel>(jsonModel);
                 return View(value);
             }
 
@@ -70,7 +70,7 @@ namespace OrkhanMammadali.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(Guid id, AboutViewModel model)
+        public async Task<IActionResult> Edit(Guid id, SosialViewModel model)
         {
             if (id != model.Id)
             {
@@ -79,7 +79,7 @@ namespace OrkhanMammadali.Controllers
             var httpclient = new HttpClient();
             var jsonModel = JsonConvert.SerializeObject(model);
             StringContent content = new StringContent(jsonModel, Encoding.UTF8, "application/json");
-            var responseMessage = await httpclient.PutAsync(url + "api/About/" + model.Id, content);
+            var responseMessage = await httpclient.PutAsync(url + "api/Sosial/" + model.Id, content);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
@@ -97,7 +97,7 @@ namespace OrkhanMammadali.Controllers
             }
 
             var httpclient = new HttpClient();
-            var responseMessage = await httpclient.DeleteAsync(url + "api/About/" + id);
+            var responseMessage = await httpclient.DeleteAsync(url + "api/Sosial/" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
